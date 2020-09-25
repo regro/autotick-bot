4a224ca29197b6cc242710e1c1c70860b5e0cf36
Matthew R. Becker <becker.mr@gmail.com>
2020-09-25 08:56:23 -0500

    ENH try this

---
+++ stop_me_if_needed.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import tempfile
+import subprocess
+import contextlib
+
+
+# https://stackoverflow.com/questions/6194499/pushd-through-os-system
+@contextlib.contextmanager
+def pushd(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
+
+
+with tempfile.TemporaryDirectory() as tmpdir, pushd(tmpdir):
+    subprocess.run(
+        ["git", "clone", "--depth=1", "https://github.com/regro/autotick-bot.git"],
+        check=True
+    )
+
+    if os.path.exists(os.path.join("circle_worker", "please.go")):
+        go = True
+    else:
+        go = False
+
+if not go:
+    print("I could not find the file 'please.go' on master! Stopping!")
+    print("::set-env name=CI_SKIP::true", flush=True)
+    sys.exit(1)
