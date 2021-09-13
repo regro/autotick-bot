4f6ca0a25c55dabb89194bd0a4b4a8fab6e0abfc
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-09-13 08:40:01 -0500

    [ci skip] ENH use mamba (#358)

--- local_setup.sh
+++ local_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda install -y -q --file=requirements.txt
+mamba install -y -q --file=requirements.txt
 
 git config --global user.name regro-cf-autotick-bot
 git config --global user.email 36490558+regro-cf-autotick-bot@users.noreply.github.com
