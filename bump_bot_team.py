8236dd268bfce9308b7fb5004ce2a17ce158ff63
Matthew R. Becker <becker.mr@gmail.com>
2021-12-27 06:42:37 -0500

    [ci skip] only bump for the bot

--- bump_bot_team.py
+++ bump_bot_team.py
@@ -3,24 +3,25 @@ import sys
 import github
 
 
-gh = github.Github(os.environ["PASSWORD"])
+if os.environ["ACTION_NAME"] == "bot":
+    gh = github.Github(os.environ["PASSWORD"])
 
-repo = gh.get_repo("regro/autotick-bot")
+    repo = gh.get_repo("regro/autotick-bot")
 
-repo.create_issue(
-    title="failed job %s" % os.environ["ACTION_NAME"],
-    body="""
-Hey @regro/auto-tick-triage!
+    repo.create_issue(
+        title="failed job %s" % os.environ["ACTION_NAME"],
+        body="""
+    Hey @regro/auto-tick-triage!
 
-It appears that the bot `%s` job failed! :(
+    It appears that the bot `%s` job failed! :(
 
-I hope it is not too much work to fix but we all know that is never the case.
+    I hope it is not too much work to fix but we all know that is never the case.
 
-Have a great day!
+    Have a great day!
 
-job url: %s
+    job url: %s
 
-""" % (os.environ["ACTION_NAME"], os.environ["ACTION_URL"])
-)
+    """ % (os.environ["ACTION_NAME"], os.environ["ACTION_URL"])
+    )
 
 sys.exit(1)
